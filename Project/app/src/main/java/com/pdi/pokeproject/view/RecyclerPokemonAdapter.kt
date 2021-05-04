10f260b4b61a7ed1b7e04ed546729f61549f77fd
spolaorthays <thays.spolaor@gmail.com>
2021-05-04 18:09:26 -0300

    Testando cores nos cards da listagem

--- Project/app/src/main/java/com/pdi/pokeproject/view/RecyclerPokemonAdapter.kt
+++ Project/app/src/main/java/com/pdi/pokeproject/view/RecyclerPokemonAdapter.kt
@@ -1,14 +1,20 @@
 package com.pdi.pokeproject.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.RecyclerView
 import com.pdi.pokeproject.R
 import com.pdi.network.data.Pokemon
 import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
+import java.lang.Exception
 
 class RecyclerPokemonAdapter: RecyclerView.Adapter<RecyclerPokemonAdapter.PokeHolder>() {
 
@@ -43,13 +49,35 @@ class RecyclerPokemonAdapter: RecyclerView.Adapter<RecyclerPokemonAdapter.PokeHo
         private var image = view.findViewById<ImageView>(R.id.pokemon_image)
         private var recyclerView = view.findViewById<RecyclerView>(R.id.recycler_types)
         private lateinit var adapter: RecyclerTypePokemonAdapter
-
-
+        private var card = view.findViewById<CardView>(R.id.pokemon_card)
 
         fun bind(pokemon: Pokemon) {
             setupRecycler()
             name.text = pokemon.name
             Picasso.get().load(pokemon.pokemonDetails.sprites.other.officialArtwotk.frontDefault).into(image)
+//            Picasso.get().load(pokemon.pokemonDetails.sprites.other.officialArtwotk.frontDefault).into(object : Target { //TODO está deixando o carrgamento bem lento
+//                override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+//                    image.setImageBitmap(bitmap)
+//                    bitmap?.let {
+//                        Palette.Builder(bitmap).generate { palette ->
+//                            palette?.vibrantSwatch?.rgb.let { paletteColor ->
+//                                paletteColor?.let { color ->
+//                                    card.setCardBackgroundColor(color)
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//
+//                override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
+//                    image.setImageDrawable(errorDrawable)
+//                }
+//
+//                override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+//                    image.setImageDrawable(placeHolderDrawable)
+//                }
+//
+//            })
             adapter.updateTypePokemons(pokemon.pokemonDetails.types)
         }
 

366acb09c240fd9593bdeb7b0c7956c1a34d30f5
spolaorthays <thays.spolaor@gmail.com>
2021-06-01 20:28:04 -0300

    Corrigido extensão

--- Project/share/src/main/java/com/pdi/share/extension/StringExtensions.kt
+++ Project/share/src/main/java/com/pdi/share/extension/StringExtensions.kt
@@ -1,7 +1,5 @@
 package com.pdi.share.extension
 
 fun String.formatFirstLetterToUpperCase(): String {
-    val firstLetter = this[0]
-    val formatFirstLetter = firstLetter.toUpperCase()
-    return formatFirstLetter+this.substring(1, this.length)
+    return this[0].toUpperCase() + this.substring(1, this.length)
 }

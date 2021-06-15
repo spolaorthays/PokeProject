14435d75c4f10ec7968281117ace6b67423d692f
spolaorthays <thays.spolaor@gmail.com>
2021-06-15 19:15:16 -0300

    Ajustes no layout base

--- Project/pokemon_list/src/main/java/com/pdi/pokemon_list/view/MainActivity.kt
+++ Project/pokemon_list/src/main/java/com/pdi/pokemon_list/view/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.pdi.pokemon_list.view
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import android.widget.ImageView
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.pdi.pokemon_list.R
 import com.pdi.pokemon_list.databinding.ActivityMainBinding
 import dagger.android.support.DaggerAppCompatActivity
@@ -41,6 +49,8 @@ class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        hideStatusBar()
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.apply {
             viewmodel = viewModel
@@ -49,6 +59,44 @@ class MainActivity : DaggerAppCompatActivity() {
 
         setupRecycler()
         setupViewModel()
+        exitApp()
+        showOptions()
+    }
+
+    private fun exitApp() {
+        val btBack = binding.toolbar.findViewById<ImageView>(R.id.button_back)
+        btBack.setOnClickListener {
+            AlertDialog.Builder(this)
+                    .setTitle(R.string.alert_title)
+                    .setMessage(R.string.alert_message_exit)
+                    .setCancelable(false)
+                    .setPositiveButton(R.string.alert_positive) { dialog, which ->
+                        dialog.dismiss()
+                        finish()
+                    }.setNegativeButton(R.string.alert_negative) { dialog, which ->
+                        dialog.dismiss()
+                    }.show()
+        }
+    }
+
+    private fun showOptions() {
+        val btMenu = binding.toolbar.findViewById<ImageView>(R.id.button_options)
+        btMenu.setOnClickListener {
+            Snackbar.make(btMenu, R.string.message_debit_function, Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    private fun hideStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.let {
+                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
     }
 
     private fun setupRecycler() {
